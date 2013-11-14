52fff320ad3ad4371f8fdc757d7ed8e589500ee0
Josh Wallace <joshwallace@hotmail.com>
2013-11-14 13:00:38 +1100

    main description fade in and out

--- sites/scripts/skills.js
+++ sites/scripts/skills.js
@@ -1,29 +1,86 @@
 function SkillsPage()
 {
+	var visibleDescription = null;
+
 	this.setupContents = function ()
 	{
-		$('.tile').each(setupHover);
+		$('.tile').each(setupTile);
+
+		$('.tile-description').each(setupTileDescription);
 
 		this.constructor.prototype.setupContents.call(this);
 	};
 
-	function setupHover(index, element)
+	function setupTile(index, element)
 	{
-		$(element).hover(skillsListHover, skillsListUnhover);
+		$(element).hover(tileHover, tileUnhover);
+
+		$(element).click(tileClick);
 	}
 
-	function skillsListHover(event)
+	function setupTileDescription(index, element)
+	{
+		$(element).click(tileDescriptionClick);
+	}
+
+	function tileHover(event)
 	{
 		var target = $($(event.target).children('.tile-short-description').filter(':not(:animated)')[0]);
 
-		target.fadeIn(500);
+		target.fadeIn(g_animationDuration);
 	}
 
-	function skillsListUnhover(event)
+	function tileUnhover(event)
 	{
-		var target = $($(event.target).children('.tile-short-description')[0]);
+		var target = $(event.target);
 
-		target.fadeOut(100);
+		if (!$(event.target).hasClass('tile-short-description'))
+		{
+			target = $(target.children('.tile-short-description')[0]);
+		}
+
+		target.fadeOut(g_animationDuration);
+	}
+
+	function tileClick(event)
+	{
+		var target = $(event.target);
+
+		if (!(target.hasClass('tile')))
+		{
+			target = $(target.parent('.tile')[0]);
+		}
+
+		target = $(target.children('.tile-description')[0]);
+
+		if (target.is(':visible'))
+		{
+			target.fadeOut(g_animationDuration);
+			visibleDescription = null;
+		}
+		else
+		{
+			if (visibleDescription != null)
+			{
+				visibleDescription.fadeOut(g_animationDuration, function () { target.fadeIn(g_animationDuration); });
+			}
+			else
+			{
+				target.fadeIn(g_animationDuration, function ()
+				{
+					target.mCustomScrollbar({ theme: 'dark-thin', horizontalScrollbar: true, autoHideScrollbar: true });
+				});
+			}
+
+			visibleDescription = target;
+
+			target.focus();
+		}
+	}
+
+	function tileDescriptionClick(event)
+	{
+		$(event.target).fadeOut(g_animationDuration);
 	}
 }
 
