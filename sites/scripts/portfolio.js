fe7e5be85c27d29e007c5952a111e01733fb67b4
Josh Wallace <joshwallace@hotmail.com>
2013-11-19 12:47:17 +1100

    tiles to portfolio

---
+++ sites/scripts/portfolio.js
@@ -0,0 +1,16 @@
+function PortfolioPage()
+{
+	this.setupContents = function ()
+	{
+		$('#scroller').tiles();
+
+		this.constructor.prototype.setupContents.call(this);
+	}
+}
+
+PortfolioPage.prototype = new Page();
+PortfolioPage.prototype.constructor = PortfolioPage;
+
+g_page = new SkillsPage();
+
+//@ sourceURL=skills.js
