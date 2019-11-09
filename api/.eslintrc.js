547015f23206b67f5d6503da869ee1cf5d5daf49
Josh Bickley-Wallace <joshbickleywallace@outlook.com>
2019-11-09 09:19:19 +0000

    Add linting

---
+++ api/.eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  env: {
+    node: true,
+  },
+  plugins: [
+    "@typescript-eslint",
+    "jest"
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    sourceType: "module",
+    project: "./tsconfig.eslint.json"
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking"
+  ],
+  rules: {
+    "semi": ["error", "always"]
+  },
+  overrides: [
+    {
+      files: ["./src/**/*.spec.ts"],
+      env: {
+        "jest/globals": true
+      },
+    },
+    {
+      files: ["./src/**/*.ts"],
+      extends: [
+        "eslint:recommended"
+      ],
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        sourceType: "module"
+      },
+      rules: {
+        "@typescript-eslint/unbound-method": ["off"]
+      }
+    }
+  ]
+};
