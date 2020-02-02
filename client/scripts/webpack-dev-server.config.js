2ccd0994e238b108f685fd5b53326fc46b6dbfb2
Josh Bickley-Wallace <joshbickleywallace@outlook.com>
2020-02-02 20:55:19 +0000

    Fix undefined public path

--- client/scripts/webpack-dev-server.config.js
+++ client/scripts/webpack-dev-server.config.js
@@ -1,15 +1,13 @@
 const paths = require("./paths");
 
 module.exports = {
-  quiet: true,
   compress: true,
   contentBase: paths.appPublic,
-  watchContentBase: true,
   hot: true,
   overlay: {
     errors: true
   },
-  watchOptions: {
-    ignored: "/node_modules/"
-  }
+  publicPath: "/",
+  quiet: true,
+  watchContentBase: true
 };
