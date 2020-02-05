d41b71f27a4150eba88a0c269e9c8f9fb07f32a6
Josh Bickley-Wallace <joshbickleywallace@outlook.com>
2020-02-05 22:39:49 +0000

    Change app name for certs

--- .devcontainer/scripts/gen-certs.sh
+++ .devcontainer/scripts/gen-certs.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 certsDir=$1
-echo $certsDir
+appName=$2
+
 mkdir $certsDir
 
 openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 \
-    -subj "/C=UK/ST=Somerset/L=Bath/O=Josh Bickley-Wallace/CN=api.bickley-wallace.com" \
+    -subj "/C=UK/ST=Somerset/L=Bath/O=Josh Bickley-Wallace/CN=$appName.bickley-wallace.com" \
     -keyout "$certsDir/private.key" -out "$certsDir/certificate.crt"
--- .devcontainer/scripts/post-create-command.sh
+++ .devcontainer/scripts/post-create-command.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 pushd "$(dirname "$0")"
 
-./gen-certs.sh "../../api/certs"
-./gen-certs.sh "../../client/certs"
+./gen-certs.sh "../../api/certs" "api"
+./gen-certs.sh "../../client/certs" "client"
 
 popd
