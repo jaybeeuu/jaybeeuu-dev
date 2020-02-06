23f79ead6ddb667311861f5a686b538da437fcb4
Josh Bickley-Wallace <joshbickleywallace@outlook.com>
2020-02-06 20:08:33 +0000

    reorder keys alphabetically

--- client/scripts/webpack-dev-server.config.js
+++ client/scripts/webpack-dev-server.config.js
@@ -5,15 +5,15 @@ const paths = require("./paths");
 module.exports = {
   compress: true,
   contentBase: paths.appPublic,
-  port: env.PORT,
+  hot: true,
   https: {
     key: fs.readFileSync(paths.certs.key),
     cert: fs.readFileSync(paths.certs.certificate),
   },
-  hot: true,
   overlay: {
     errors: true
   },
+  port: env.PORT,
   quiet: true,
-  watchContentBase: true
+  watchContentBase: true,
 };
