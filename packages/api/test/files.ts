467b431932c2e7500da84739ab83ce4cff3f54d6
Josh Bickley-Wallace <joshbickleywallace@outlook.com>
2020-03-17 22:56:39 +0000

    recursively copy files

--- packages/api/test/files.ts
+++ packages/api/test/files.ts
@@ -1,6 +1,7 @@
-import fs from"fs";
+import { promises as fs } from"fs";
 import rimraf from "rimraf";
 import { POST_DIST_DIRECTORY, POST_REPO_DIRECTORY, REMOTE_POST_REPO_DIRECTORY } from "./mock-env";
+import path from "path";
 
 const rimrafPromise = (path: string): Promise<void> => new Promise((resolve, reject) => {
   rimraf(path, (error) => {
@@ -17,20 +18,24 @@ export const cleanUpFiles = (): Promise<[void, void, void]> => Promise.all([
   rimrafPromise(REMOTE_POST_REPO_DIRECTORY)
 ]);
 
-var copyDir = function(sourcePath: string, destinationPath: string) {
-	fs.mkdtemp(destinationPath)
-	var files = fs.readdirSync(sourcePath);
-	files.forEach((file) => {
-		var current = fs.lstatSync(path.join(sourcePath, files[i]));
-		if(current.isDirectory()) {
-			copyDir(path.join(sourcePath, files[i]), path.join(destinationPath, files[i]));
-		} else if(current.isSymbolicLink()) {
-			var symlink = fs.readlinkSync(path.join(sourcePath, files[i]));
-			fs.symlinkSync(symlink, path.join(destinationPath, files[i]));
+export const copyDir = async (sourcePath: string, destinationPath: string): Promise<void> => {
+	await fs.mkdir(destinationPath);
+
+  const files = await fs.readdir(sourcePath);
+  await Promise.all(files.map(async (file) => {
+    const fileStats = await fs.lstat(path.join(sourcePath, file));
+    const sourceFile =  path.join(sourcePath, file);
+    const destinationFile =  path.join(destinationPath, file);
+
+		if (fileStats.isDirectory()) {
+			await copyDir(sourceFile, destinationFile);
+		} else if(fileStats.isSymbolicLink()) {
+			const symlink = await fs.readlink(sourceFile);
+			await fs.symlink(symlink, destinationFile);
 		} else {
-			copy(path.join(sourcePath:, files[i]), path.join(destinationPath, files[i]));
+			await fs.copyFile(sourceFile, destinationFile);
 		}
-	}
+	}));
 };
 
 export const setupDirectories = ({
@@ -38,12 +43,12 @@ export const setupDirectories = ({
   postRepoSourceDirectory,
   remotePostRepoSourceDirectory
 }: {
-  postDistDirectory: string,
-  postRepoDirectory: string,
-  remotePostRepoDirectory: string
+  postDistSourceDirectory: string,
+  postRepoSourceDirectory: string,
+  remotePostRepoSourceDirectory: string
 }): Promise<[void, void, void]> => Promise.all([
-  fs.(POST_DIST_DIRECTORY),
-  rimrafPromise(POST_REPO_DIRECTORY),
-  rimrafPromise(REMOTE_POST_REPO_DIRECTORY)
+  copyDir(postDistSourceDirectory, POST_DIST_DIRECTORY),
+  copyDir(postRepoSourceDirectory, POST_REPO_DIRECTORY),
+  copyDir(remotePostRepoSourceDirectory, REMOTE_POST_REPO_DIRECTORY)
 ]);
 
--- packages/api/test/mock-env.ts
+++ packages/api/test/mock-env.ts
@@ -11,7 +11,6 @@ export {
 } from "../src/env";
 
 jest.mock("../src/env", () => {
-  const path = import("path");
   const jestWorkerId = +(process.env.JEST_WORKER_ID || 0);
   const REMOTE_POST_REPO_DIRECTORY = "./fs/test/remote";
   return {
@@ -20,10 +19,10 @@ jest.mock("../src/env", () => {
     CLIENT_HOST_NAME: "localhost",
     CLIENT_PORT: 5237 + jestWorkerId,
     NODE_ENV: process.env.NODE_ENV,
-    POST_REPO_DIRECTORY: path.join("./fs/test/local", jestWorkerId.toString(10)),
-    POST_REPO_REMOTE: path.join(REMOTE_POST_REPO_DIRECTORY, ".git"),
+    POST_REPO_DIRECTORY: `./fs/test/local/${jestWorkerId.toString(10)}`,
+    POST_REPO_REMOTE: `${REMOTE_POST_REPO_DIRECTORY}/.git`,
     REMOTE_POST_REPO_DIRECTORY,
-    POST_DIST_DIRECTORY: path.join("./fs/test/dist", jestWorkerId.toString(10)),
+    POST_DIST_DIRECTORY: `./fs/test/dist/${jestWorkerId.toString(10)}`,
   };
 });
 
