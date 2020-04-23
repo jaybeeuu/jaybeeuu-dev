99ca57408039111d8cf3b45692577b323d375f33
Josh Bickley-Wallace <joshbickleywallace@outlook.com>
2020-04-23 20:31:19 +0000

    Alter code style

--- packages/api/src/routes/with-handled-errors.ts
+++ packages/api/src/routes/with-handled-errors.ts
@@ -2,15 +2,16 @@ import { RequestHandler } from "express";
 import log from "../log";
 import { NODE_ENV } from "../env";
 
-export const withHandledErrors = (handler: RequestHandler): RequestHandler =>
-  async (req, res, next) => {
-    try {
-      await handler(req, res, next);
-    } catch (error) {
-      log.error(error);
-      res.status(500).json({
-        message: error.message || error,
-        stack: NODE_ENV === "production" ? undefined : error.stack
-      });
-    }
-  };
+export const withHandledErrors = (
+  handler: RequestHandler
+): RequestHandler => async (req, res, next) => {
+  try {
+    await handler(req, res, next);
+  } catch (error) {
+    log.error(error);
+    res.status(500).json({
+      message: error.message || error,
+      stack: NODE_ENV === "production" ? undefined : error.stack
+    });
+  }
+};
