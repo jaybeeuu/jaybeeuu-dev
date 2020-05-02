161d8edccdacd2820313bccdda93d68b2fd926aa
Josh Bickley-Wallace <joshbickleywallace@outlook.com>
2020-05-02 21:14:09 +0000

    Try to have it replace the placeholder

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -1,6 +1,14 @@
 import { h, render } from "preact";
 import App from "./app/app";
 
-render((
-  <App />
-), document.getElementById("app"));
+const appSelector = "#app";
+const app = document.querySelector(appSelector);
+
+const placeholderSelector = `${appSelector} .placeholder`;
+const placeholder = document.querySelector(placeholderSelector);
+
+if (!app || !placeholder) {
+  throw new Error(`Unable to start app; missing element "${appSelector}" or "${placeholderSelector}"`);
+}
+
+render(<App />, app, placeholder);
