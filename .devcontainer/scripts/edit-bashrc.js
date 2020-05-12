2084432a4c25148807703c7575cfd84d85ef7d31
Josh Bickley-Wallace <joshbickleywallace@outlook.com>
2020-05-12 06:15:15 +0000

    reinstate devcontainer bashrc edits

--- .devcontainer/scripts/edit-bashrc.js
+++ .devcontainer/scripts/edit-bashrc.js
@@ -5,18 +5,15 @@ const oldBaschrc = path.join(process.env["HOME"], ".bashrc");
 const newBaschrc = path.join(process.env["HOME"], ".newbashrc");
 
 const replaceLines = {
-  "    PS1='${debian_chroot:+($debian_chroot)}\\[\\033[01;32m\\]\\u@\\h\\[\\033[00m\\]:\\[\\033[01;34m\\]\\w\\[\\033[00m\\]\\$ '":
-  "    PS1=\"${debian_chroot:+($debian_chroot)}\\n\\[\\033[01;32m\\]\\u@\\h\\[\\033[00m\\]:\\[\\033[01;34m\\]\\w\\[\\033[00m\\]\\[\\033[33m\\]\\$(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \\(.*\\)/ (\\1)/')\\n\\[\\033[0m\\033[0;32m\\] \\$\\[\\033[0m\\] \"",
-
-  "    PS1='${debian_chroot:+($debian_chroot)}\\u@\\h:\\w\\$ '":
-  "    PS1=\"${debian_chroot:+($debian_chroot)}\\n\\u@\\h:\\w\\$(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \\(.*\\)/ (\\1)/')\\n \\$ \""
+  60: "    PS1=\"${debian_chroot:+($debian_chroot)}\\n\\[\\033[01;32m\\]\\u@\\h\\[\\033[00m\\]:\\[\\033[01;34m\\]\\w\\[\\033[00m\\]\\[\\033[33m\\]\\$(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \\(.*\\)/ (\\1)/')\\n\\[\\033[0m\\033[0;32m\\] \\$\\[\\033[0m\\] \"",
+  62: "    PS1=\"${debian_chroot:+($debian_chroot)}\\n\\u@\\h:\\w\\$(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \\(.*\\)/ (\\1)/')\\n \\$ \""
 };
 
 const newBashrcWriteStream = fs.createWriteStream(newBaschrc, { flags: 'a' });
 
-const getLineToWrite = (line) => {
-  if (replaceLines.hasOwnProperty(line)) {
-    return `#${line}\n${replaceLines[line]}`;
+const getLineToWrite = (line, lineNumber) => {
+  if (replaceLines.hasOwnProperty(lineNumber)) {
+    return `#${line}\n${replaceLines[lineNumber]}`;
   } else {
     return line;
   }
@@ -24,8 +21,8 @@ const getLineToWrite = (line) => {
 
 const oldData = fs.readFileSync(oldBaschrc, 'UTF8');
 
-oldData.split('\n').forEach((lineFromFile) => {
-  const lineToWrite = getLineToWrite(lineFromFile);
+oldData.split('\n').forEach((lineFromFile, lineIndex) => {
+  const lineToWrite = getLineToWrite(lineFromFile, lineIndex + 1);
   newBashrcWriteStream.write(`${lineToWrite}\n`);
 });
 
