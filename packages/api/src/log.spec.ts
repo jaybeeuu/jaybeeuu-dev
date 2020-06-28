3c3f91ce6fa3634cad3022398856f91f2cc736d0
Josh Bickley-Wallace <joshbickleywallace@outlook.com>
2020-06-28 07:01:48 +0100

    Better error messages

--- packages/api/src/log.spec.ts
+++ packages/api/src/log.spec.ts
@@ -11,24 +11,38 @@ describe("log", () => {
   describe("info", () => {
     it("passes arguments to console.log", () => {
       const args = [1, "2", { id: 3 }];
-      log.info(...args);
+      log.info("message", ...args);
 
-      expect(console.log).toHaveBeenCalledWith(...args);
+      expect(console.log).toHaveBeenCalledWith("message", ...args);
     });
   });
 
   describe("error", () => {
-    it("passes arguments to console.error", () => {
+    it("formats a message containing message and stack if supplied.", () => {
+      const message = "{message}";
       const error = {
-        name: "{name}",
         message: "{message}",
         stack: "{stack}"
       };
 
-      log.error(error);
+      log.error(message, error);
 
       expect(console.error).toHaveBeenCalledWith(expect.stringContainingAll(
-        error.name,
+        message,
+        error.message,
+        error.stack
+      ));
+    });
+
+    it("when passed an Error the message and stack are sent onto the console", () => {
+      const message = "{message}";
+      const error = new Error("{error message}");
+      error.stack = "{stack}";
+
+      log.error("message", error);
+
+      expect(console.error).toHaveBeenCalledWith(expect.stringContainingAll(
+        message,
         error.message,
         error.stack
       ));
--- packages/api/src/log.ts
+++ packages/api/src/log.ts
@@ -1,7 +1,21 @@
 // eslint-disable-next-line no-console
-export const info = (...args: any[]): void => console.log(...args);
+export const info = (message: string, ...args: unknown[]): void => console.log(message, ...args);
+
+const getErrorMessage = (err: any | null | undefined): string => {
+  if (!err) {
+    return JSON.stringify(err) || "Empty string";
+  }
+
+  if (typeof err === 'string') {
+    return err;
+  }
+
+  return `${err.message ?? JSON.stringify(err)}\n\n${err.stack ?? "Error: No stack."}`;
+};
 
 // eslint-disable-next-line no-console
-export const error = (err: Error): void => console.error(
-  `Error (${err.name}):\n${err.message}\n\n${err.stack}`
-);
+export const error = (message:string, err?: unknown): void => {
+  const errorMessage = getErrorMessage(err);
+
+  console.error(`${message}: ${errorMessage}`);
+}
