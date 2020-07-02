f924b374fef0f52ddde264eb7c0788f82c11b711
Josh Bickley-Wallace <joshbickleywallace@outlook.com>
2020-07-02 16:13:45 +0100

    Improve error handling when remote clone fails.

--- packages/api/src/repo/src/update.ts
+++ packages/api/src/repo/src/update.ts
@@ -8,7 +8,18 @@ export const update = async (): Promise<void> => {
   if (await canAccess(POSTS_REPO_DIRECTORY)) {
     await git(POSTS_REPO_DIRECTORY).pull();
   } else {
+    const remoteRepoPath = resolveApp(REMOTE_POST_REPO);
+
+    if (!await canAccess(remoteRepoPath)) {
+      throw new Error(`Remote repo does not exist: ${remoteRepoPath}`);
+    }
+
     await fs.promises.mkdir(POSTS_REPO_DIRECTORY, { recursive: true });
-    await git(POSTS_REPO_DIRECTORY).clone(resolveApp(REMOTE_POST_REPO), POSTS_REPO_DIRECTORY);
+    try {
+      await git(POSTS_REPO_DIRECTORY).clone(remoteRepoPath, POSTS_REPO_DIRECTORY);
+    } catch (err) {
+      await fs.promises.rmdir(POSTS_REPO_DIRECTORY, { recursive: true });
+      throw err;
+    }
   }
 };
