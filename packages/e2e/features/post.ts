034fc628b389d1f7d3e2e35578db622df88d4583
Josh Bickley-Wallace <joshbickleywallace@outlook.com>
2020-09-14 22:33:30 +0100

    E"E tests actually look for the post text.

--- packages/e2e/features/post.ts
+++ packages/e2e/features/post.ts
@@ -1,5 +1,6 @@
+
 import { makeClassSelectors, post } from "@bickley-wallace/e2e-hooks";
-import { PostSlug } from "../routes/posts";
+import { PostSlug, withPostMetaData } from "../routes/posts";
 
 const mainPanelSelectors = makeClassSelectors(post);
 
@@ -7,8 +8,34 @@ export const get = (): Cypress.Chainable<JQuery<HTMLElement>> => {
   return cy.get(mainPanelSelectors.block);
 };
 
-export const getArticle = (): Cypress.Chainable<JQuery<HTMLElement>> => {
-  return cy.get(mainPanelSelectors.article);
+export type ArticleShould = Cypress.Chainer<JQuery<HTMLElement>> & {
+  (chainer: "contain.post", slug: PostSlug): Cypress.Chainable<JQuery<HTMLElement>>
+};
+
+export const getArticle = (): Cypress.Chainable<JQuery<HTMLElement>> & {
+  should: ArticleShould
+} => {
+  // eslint-disable-next-line cypress/no-assigning-return-values
+  const article = cy.get(mainPanelSelectors.article).as("article");
+
+  const articleShould: ArticleShould = (
+    chainer: any, ...args: any[]
+  ): Cypress.Chainable<JQuery<HTMLElement>> => {
+    if (chainer === "contain.post") {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      const [slug] = args;
+      return withPostMetaData(slug).then((meta) =>{
+        return cy.fixture(`posts/${meta.fileName}`).then((postContent) => {
+          return cy.get(mainPanelSelectors.article).should("contain.html", postContent);
+        });
+      });
+    }
+    return cy.get(mainPanelSelectors.article).should(chainer, ...args);
+  };
+
+  article.should = articleShould;
+
+  return article;
 };
 
 export const navigateTo = (slug: PostSlug): void => {
