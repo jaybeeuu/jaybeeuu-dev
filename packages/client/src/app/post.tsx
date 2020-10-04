a27dc511e0c9cdfe6335e11c47c7b002245723df
Josh Bickley-Wallace <joshbickleywallace@outlook.com>
2020-10-04 21:43:27 +0100

    Darken background on code

--- packages/client/src/app/post.tsx
+++ packages/client/src/app/post.tsx
@@ -1,15 +1,19 @@
-import { post as postHooks } from "@bickley-wallace/e2e-hooks";
+import classNames from "classnames";
 import { h, FunctionComponent, JSX } from "preact";
+import { post as postHooks } from "@bickley-wallace/e2e-hooks";
 import { withPromise } from "./with-promise";
 import { asRoute } from "./as-route";
 
-import "./night-owl.css";
 import { useValue } from "../recoilless/use-value";
 import { currentPostHtml, currentPostSlug } from "./state";
 import { useEffect } from "preact/hooks";
 
+import css from "./post.module.css";
+
+import "./night-owl.css";
+
 const PostComponent = withPromise(({ value: post }: { value: string }): JSX.Element => (
-  <article className={postHooks.article} dangerouslySetInnerHTML={{ __html: post }}/>
+  <article className={classNames(css.element, postHooks.article)} dangerouslySetInnerHTML={{ __html: post }}/>
 ));
 
 export type PostProps = { path: string, slug: string };
@@ -19,6 +23,7 @@ const Post: FunctionComponent<PostProps> = ({ slug }) => {
   useEffect(() => {
     setSlug(slug);
   }, [slug]);
+
   if (!selectedSlug) {
     return null;
   }
