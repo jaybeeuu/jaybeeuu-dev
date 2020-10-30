a553143db30efbfd71d7a537cb2d486a64e83d2f
Josh Bickley-Wallace <joshbickleywallace@outlook.com>
2020-10-30 21:24:57 +0000

    Update use\Vlaue following type change

--- packages/client/src/recoilless/use-value.ts
+++ packages/client/src/recoilless/use-value.ts
@@ -1,28 +1,35 @@
 import { useContext, useEffect, useMemo, useState } from "preact/hooks";
 import { useAsyncGenerator } from "./async-hooks";
 import { monitorPromise, PromiseState } from "./promise-status";
-import { DerivedValue, isPrimitiveValue, PrimitiveValueState, PrimitiveValue, Value } from "./state";
+import { DerivedValue, isPrimitiveValue, PrimitiveValue, Value, ValueState } from "./state";
+import { Store } from "./store";
 import { StoreContext } from "./store-provider";
 
-export function useValue<Val>(value: DerivedValue<Promise<Val>>): PromiseState<Val>;
-export function useValue<Val>(value: DerivedValue<Val>): Val;
-export function useValue<Val>(value: PrimitiveValue<Val>): [Val, (newValue: Val) => void];
-export function useValue<Val>(
-  value: Value<Val | Promise<Val>>
-): Val | PromiseState<Val> | [Val, (newValue: Val) => void] {
-  const [, updateState] = useState({});
-  const store = useContext(StoreContext);
-  const valueState = store.getValue(value);
+const useStore = (): Store => {
+  return useContext(StoreContext);
+};
 
+const useValueSubscription = <Val>(valueState: ValueState<Val>): void => {
+  const [, updateState] = useState({});
   useEffect(() => {
     const unsubscribe = valueState.subscribe(() => updateState({}));
     return () => unsubscribe();
-  }, [value]);
+  }, [valueState]);
+};
 
-  if (isPrimitiveValue(value)) {
-    const primitiveValue = valueState as PrimitiveValueState<Val>;
-    return [primitiveValue.current, primitiveValue.setValue];
-  }
+const usePrimitiveValue = <Val>(
+  value: PrimitiveValue<Val>
+): [Val, (newValue: Val) => void] => {
+  const valueState = useStore().getValue(value);
+  useValueSubscription(valueState);
+  return [valueState.current, valueState.setValue];
+};
+
+const useDerivedValue = <Val>(
+  value: DerivedValue<Val>
+): Val | PromiseState<Val> => {
+  const valueState = useStore().getValue(value);
+  useValueSubscription(valueState);
 
   const current = valueState.current;
   if (current instanceof Promise) {
@@ -31,4 +38,17 @@ export function useValue<Val>(
   }
 
   return current;
+};
+
+export function useValue<Val>(value: DerivedValue<Promise<Val>>): PromiseState<Val>;
+export function useValue<Val>(value: DerivedValue<Val>): Val;
+export function useValue<Val>(value: PrimitiveValue<Val>): [Val, (newValue: Val) => void];
+export function useValue<Val>(
+  value: Value<Val>
+): Val | PromiseState<Val> | [Val, (newValue: Val) => void] {
+
+  if (isPrimitiveValue(value)) {
+    return usePrimitiveValue(value);
+  }
+  return useDerivedValue(value);
 }
