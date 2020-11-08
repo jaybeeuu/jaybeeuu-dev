e18dde764f9b93bbd5e939c6b4291de115496b01
Josh Bickley-Wallace <joshbickleywallace@outlook.com>
2020-11-08 21:35:13 +0000

    correct exports of echo utils

--- packages/utilities/src/index.ts
+++ packages/utilities/src/index.ts
@@ -1,6 +1,6 @@
 export { debounce } from "./debounce";
 export { hasOwnProperty, hasPropertyOfType } from "./type-guards";
 export * as log from "./log";
-export { delay, echo as echoDelayed, clearableEcho as clearableDelayedEcho } from "./delay";
+export { delay, echo, clearableEcho } from "./delay";
 export { joinUrlPath } from "./join-url-path";
 export { multiPartition } from "./multi-partition";
