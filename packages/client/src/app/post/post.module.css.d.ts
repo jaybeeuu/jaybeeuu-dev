b5c9308528906634ae0d6c250287a9082fea8323
Josh Bickley-Wallace <joshbickleywallace@outlook.com>
2020-11-27 22:33:49 +0000

    content variables

--- packages/client/src/app/post/post.module.css.d.ts
+++ packages/client/src/app/post/post.module.css.d.ts
@@ -2,7 +2,6 @@
 // Please do not change this file!
 interface CssExports {
   'componentRoot': string;
-  'titleContainer': string;
 }
 export const cssExports: CssExports;
 export default cssExports;
