f993e6f722df57548ad4fb9cca1884fc2f32a5c9
Josh Bickley-Wallace <joshbickleywallace@outlook.com>
2021-05-16 20:53:39 +0100

    Update all deps

--- packages/e2e/plugins/index.js
+++ packages/e2e/plugins/index.js
@@ -1,11 +1,11 @@
-const webpackPreprocessor = require("@cypress/webpack-preprocessor");
-const webpackOptions = require("../webpack.config");
+// const webpackPreprocessor = require("@cypress/webpack-preprocessor");
+// const webpackOptions = require("../webpack.config");
 
-module.exports = (on) => {
-  const options = { webpackOptions };
-  const preprocessor = webpackPreprocessor(options);
+// module.exports = (on) => {
+//   const options = { webpackOptions };
+//   const preprocessor = webpackPreprocessor(options);
 
-  on("file:preprocessor", function (...args) {
-    return preprocessor(...args);
-  });
-};
+//   on("file:preprocessor", function (...args) {
+//     return preprocessor(...args);
+//   });
+// };
