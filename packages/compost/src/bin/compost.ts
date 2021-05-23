90335e14a1e1293c866c135cdae81e536bb62fab
Josh Bickley-Wallace <joshbickleywallace@outlook.com>
2021-05-23 20:38:22 +0100

    Use yaargs for compost

--- packages/compost/src/bin/compost.ts
+++ packages/compost/src/bin/compost.ts
@@ -1,33 +1,14 @@
 import { debounce, log } from "@bickley-wallace/utilities";
 import chokidar from "chokidar";
-import getOpts from "getopts";
+import yargs from "yargs";
 import { update } from "../posts";
 import { UpdateOptions } from "../posts/src/types";
 import { UpdateFailureReason } from "../posts/src/update";
 import { Result, success, failure } from "../results";
 
-const options = getOpts(process.argv, {
-  alias: {
-    additionalWatchPaths: "a",
-    hrefRoot: "h",
-    manifestFileName: "m",
-    outputDir: "o",
-    sourceDir: "s",
-    watch: "w",
-    includeUnpublished: "u"
-  },
-  default: {
-    additionalWatchPaths: "",
-    hrefRoot: "/",
-    manifestFileName: "manifest.json",
-    outputDir: "./lib",
-    sourceDir: "./src",
-    watch: false,
-    includeUnpublished: false
-  }
-}) as unknown as UpdateOptions;
-
-const run = async (): Promise<Result<never, "error" | UpdateFailureReason>> => {
+const run = async (
+  options: UpdateOptions
+): Promise<Result<never, "error" | UpdateFailureReason>> => {
   try {
     log.info("Composting...");
     const result = await update(options);
@@ -48,26 +29,112 @@ const run = async (): Promise<Result<never, "error" | UpdateFailureReason>> => {
   }
 };
 
-const watch = (): void => {
+const watch = (
+  options: UpdateOptions
+): void => {
   log.info("Starting compost in watch mode...");
   const debouncedRun = debounce(async () => {
-    await run();
+    await run(options);
     log.info("Waiting for changes...");
   }, 250);
   const watchPaths = [
     options.sourceDir,
-    ...options.additionalWatchPaths.split(",").map((path) => path.trim())
+    ...options.additionalWatchPaths
   ].filter(Boolean);
   chokidar.watch(watchPaths).on("all", debouncedRun);
 };
 
-if (options.watch) {
-  void watch();
-} else {
-  void (async () => {
-    const result = await run();
-    process.exit(
-      result.success ? 0 : 1
-    );
-  })();
+interface CompostCommandArgs {
+  "additional-watch-paths"?: string[];
+  "href-root": string;
+  "manifest-file-name": string;
+  "output-dir": string;
+  "source-dir": string;
+  "watch": boolean;
+  "include-unpublished": boolean;
 }
+
+const mapArgsToOptions = (args: CompostCommandArgs): UpdateOptions => ({
+  additionalWatchPaths: args["additional-watch-paths"] ?? [],
+  hrefRoot: args["href-root"],
+  manifestFileName: args["manifest-file-name"],
+  outputDir: args["output-dir"],
+  sourceDir: args["source-dir"],
+  watch: args["watch"],
+  includeUnpublished: args["include-unpublished"],
+});
+
+yargs.command(
+  "$0",
+  "Here we go!",
+  {
+    "href-root": {
+      type: "string",
+      default: "/",
+      alias: ["r"],
+      description: "The root path to apply when compiling hrefs (e.g. links)."
+    },
+    "additional-watch-paths": {
+      alias: ["a"],
+      description: "Paths other than --source-dir to watch when in watch mode.",
+      implies: "watch",
+      type: "string",
+      array: true
+    },
+    "include-unpublished": {
+      alias: ["u"],
+      description: "Whether or not to compile posts not marked as published in their metadata.json file.",
+      type: "boolean",
+      default: false
+    },
+    "manifest-file-name": {
+      alias: ["m"],
+      description: "The nam of the output JSON manifest file.",
+      type: "string",
+      default: "manifest.json"
+    },
+    "output-dir": {
+      alias: ["o"],
+      description: "The directory into which the compiled files should be written.",
+      type: "string",
+      default: "./lib"
+    },
+    "source-dir": {
+      alias: ["s"],
+      description: "The directory containing the source files.",
+      type: "string",
+      default: "./src"
+    },
+    "watch": {
+      alias: ["w"],
+      description: "Watch the source files and recompile the posts when changes occur.",
+      type: "boolean",
+      default: false
+    }
+  },
+  async (args) => {
+    const options = mapArgsToOptions(args);
+
+    if (options.watch) {
+      watch(options);
+    } else {
+      const result = await run(options);
+
+      if (result.success) {
+        log.info("Success!");
+        yargs.exit(0, new Error("Success!"));
+      } else {
+        log.info(`Failed :(\n${result.message}`);
+        yargs.exit(1, result);
+      }
+    }
+  }
+);
+
+yargs.help("help")
+  .alias("h", "help")
+  .showHelpOnFail(false, "Specify --help for available options");
+yargs.demandCommand();
+yargs.recommendCommands();
+yargs.strict();
+yargs.parse();
