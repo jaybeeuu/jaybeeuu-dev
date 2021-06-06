e518b22169c277293e4a1e28c3378afa4b11f31a
Josh Bickley-Wallace <joshbickleywallace@outlook.com>
2021-06-06 21:17:48 +0100

    e2e hooks use template string types

--- packages/e2e-hooks/src/make-hooks.ts
+++ packages/e2e-hooks/src/make-hooks.ts
@@ -1,22 +1,55 @@
-export type HookBlock = {
-  (): string;
-  element: (element: string) => string;
-  childBlock: (block: string) => HookBlock
-  modifier: (modifier: string) => string;
+export type ElementIdentifier<Block extends string, Element extends string> = `${Block}__${Element}`
+export type ModifierIdentifier<Block extends string, Modifier extends string> = `${Block}--${Modifier}`
+
+export type HookBlock<Prefix extends string, Block extends string> = {
+  (): ElementIdentifier<Prefix, Block>;
+  element: <Element extends string>(element: Element) => ElementIdentifier<ElementIdentifier<Prefix, Block>, Element>;
+  childBlock: <NewBlock extends string>(block: NewBlock) => HookBlock<ElementIdentifier<Prefix, Block>, NewBlock>
+  modifier: <Modifier extends string>(modifier: Modifier) => ModifierIdentifier<ElementIdentifier<Prefix, Block>, Modifier>;
+  makeModifierFactory: () => <Modifier extends string>(modifier: Modifier) => ModifierIdentifier<ElementIdentifier<Prefix, Block>, Modifier>;
 }
 
-const hookBlockFactory = (prefix: string) => (block: string): HookBlock => {
-  const blockClass = `${prefix}__${block}`;
-  const getBlock = (): string => blockClass;
+const hookBlockFactory = <Prefix extends string>(
+  prefix: Prefix
+) => <Block extends string>(
+  block: Block
+): HookBlock<Prefix, Block> => {
+  const blockClass = `${prefix}__${block}` as const;
+
+  const getBlock: HookBlock<Prefix, Block> = (): ElementIdentifier<Prefix, Block> => {
+    return blockClass;
+  };
+
   getBlock.childBlock = hookBlockFactory(blockClass);
-  getBlock.element = (element: string) => `${blockClass}__${element}`;
-  getBlock.modifier = (modifier: string) => `${blockClass}--${modifier}`;
+
+  getBlock.element = <Element extends string>(
+    element: Element
+  ): ElementIdentifier<typeof blockClass, Element> => {
+    return `${blockClass}__${element}` as const;
+  };
+
+  getBlock.makeModifierFactory = () => <Modifier extends string>(
+    modifier: Modifier
+  ): ModifierIdentifier<typeof blockClass, Modifier> => {
+    return `${blockClass}--${modifier}` as const;
+  };
+
+  getBlock.modifier = getBlock.makeModifierFactory();
+
   return getBlock;
 };
 
-export const makeHookBlock = (block: string): HookBlock => hookBlockFactory("e2e")(block);
+export const makeHookBlock = <Block extends string>(
+  block: Block
+): HookBlock<"e2e", Block> => {
+  return hookBlockFactory("e2e")(block);
+};
 
-const asClassSelector = (identifier: string): string => `.${identifier}`;
+const asClassSelector = <Identifier extends string>(
+  identifier: Identifier
+): `.${Identifier}` => {
+  return `.${identifier}` as const;
+};
 
 export type Hook = string | ((identifier: string) => string) | HookMap;
 
@@ -24,11 +57,22 @@ export type HookMap = {
   [element: string]: Hook;
 };
 
-export const makeClassSelectors = <Hooks extends HookMap>(hooks: Hooks): Hooks => {
-  return Object.entries(hooks).reduce<{ [hook: string]: Hook }>((acc, [name, hook]) => {
+type ClassSelectors<Hooks extends HookMap> = {
+  [Key in keyof Hooks]: Hooks[Key] extends string
+    ? `.${Hooks[Key]}`
+    : Hooks[Key] extends (...args: any) => any
+      ? (...args: Parameters<Hooks[Key]>) => `.${ReturnType<Hooks[Key]>}`
+      : ClassSelectors<Extract<Hooks[Key], HookMap>>;
+}
+
+export const makeClassSelectors = <Hooks extends HookMap>(hooks: Hooks): ClassSelectors<Hooks> => {
+  return Object.entries(hooks).reduce<Record<string, unknown>>((
+    acc,
+    [name, hook]
+  ) => {
     switch(typeof hook) {
       case "function": {
-        acc[name] = (identifier: string): string => asClassSelector(hook(identifier));
+        acc[name] = (identifier: string) => asClassSelector(hook(identifier));
         break;
       }
       case "object": {
@@ -43,5 +87,5 @@ export const makeClassSelectors = <Hooks extends HookMap>(hooks: Hooks): Hooks =
     }
 
     return acc;
-  }, { }) as Hooks;
+  }, { }) as ClassSelectors<Hooks>;
 };
