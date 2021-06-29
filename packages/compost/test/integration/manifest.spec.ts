d16a6a3208ddc398230b67f1de48246f1b3fd66a
Josh Bickley-Wallace <joshbickleywallace@outlook.com>
2021-06-29 21:11:06 +0100

    Add unitet tests for last updated time.

--- packages/compost/test/integration/manifest.spec.ts
+++ packages/compost/test/integration/manifest.spec.ts
@@ -1,3 +1,5 @@
+import type {
+  PostFile} from "./helpers";
 import {
   compilePosts,
   getPostManifest,
@@ -7,11 +9,15 @@ import {
 } from "./helpers";
 
 import { advanceTo } from "jest-date-mock";
+import fetch from "node-fetch";
+import { mocked } from "ts-jest/utils";
+
 import {
   deleteDirectories,
-  writeTextFiles
 } from "../../src/files/index";
 import type { PostMetaFileData } from "../../src/posts/src/metafile";
+
+jest.mock("node-fetch");
 
 describe("manifest", () => {
   it("has an entry for a new post with the correct properties.", async () => {
@@ -30,8 +36,8 @@ describe("manifest", () => {
       meta,
       content: "# This is the first post\n\nIt has some content."
     });
-    const hrefRoot = "posts";
-    await compilePosts({ hrefRoot });
+
+    await compilePosts();
 
     const manifest = await getPostManifest();
 
@@ -42,46 +48,152 @@ describe("manifest", () => {
         lastUpdateDate: null,
         slug,
         fileName: expect.stringMatching(new RegExp(`${slug}-[A-z0-9]{6}.html`)) as unknown,
-        href: expect.stringMatching(new RegExp(`/${hrefRoot}/${slug}-[A-z0-9]{6}.html`)) as unknown
+        href: expect.stringMatching(new RegExp(`/posts/${slug}-[A-z0-9]{6}.html`)) as unknown
       }
     });
   });
 
-  it("has an entry for each new post with the correct properties.", async () => {
+  it("compiles the posts to the specified hrefRoot when one is supplied..", async () => {
     await deleteDirectories(sourceDir, outputDir);
 
-    const publishDate = "2020-03-11";
-    advanceTo(publishDate);
     const slug = "first-post";
-    const meta: PostMetaFileData = {
-      title: "This is the first post",
-      abstract: "This is the very first post.",
-      publish: true
-    };
-    await writeTextFiles(
-      sourceDir,
-      [{
-        path: `./${slug}.md`,
-        content: "# This is the first post\n\nIt has some content."
-      }, {
-        path: `./${slug}.post.json`,
-        content: JSON.stringify(meta, null, 2)
-      }]
-    );
+    await writePostFiles({
+      slug,
+      meta: {
+        title: "This is the first post",
+        abstract: "This is the very first post.",
+        publish: true
+      },
+      content: "# This is the first post\n\nIt has some content."
+    });
     const hrefRoot = "posts";
     await compilePosts({ hrefRoot });
 
     const manifest = await getPostManifest();
 
-    expect(manifest).toStrictEqual({
-      [slug]: {
-        ...meta,
-        publishDate: new Date(publishDate).toUTCString(),
-        lastUpdateDate: null,
-        slug,
-        fileName: expect.stringMatching(new RegExp(`${slug}-[A-z0-9]{6}.html`)) as unknown,
-        href: expect.stringMatching(new RegExp(`/${hrefRoot}/${slug}-[A-z0-9]{6}.html`)) as unknown
-      }
+    expect(manifest[slug].href).toStrictEqual(
+      expect.stringMatching(new RegExp(`/${hrefRoot}/${slug}-[A-z0-9]{6}.html`)) as unknown
+    );
+  });
+
+  it("updates the lastUpdatedDate when a post is updated and recompiled, and compost has access to the old manifest.", async () => {
+    await deleteDirectories(sourceDir, outputDir);
+
+    const slug = "first-post";
+    const postFile: PostFile = {
+      slug,
+      meta: {
+        title: "This is the first post",
+        abstract: "This is the very first post.",
+        publish: true
+      },
+      content: [
+        "# This is the first post",
+        "It has some content."
+      ]
+    };
+    await writePostFiles(postFile);
+
+    const publishDate = "2020-03-11";
+    advanceTo(publishDate);
+    await compilePosts();
+
+    await writePostFiles({
+      ...postFile,
+      content: [
+        ...postFile.content,
+        "some new content"
+      ]
     });
+
+    const updatedDate = "2020-03-12";
+    advanceTo(updatedDate);
+    await compilePosts();
+
+    const manifest = await getPostManifest();
+
+    expect(manifest[slug].lastUpdateDate).toStrictEqual(
+      new Date(updatedDate).toUTCString()
+    );
+  });
+
+  it("does not add an updated date if the post hsa not updated.", async () => {
+    await deleteDirectories(sourceDir, outputDir);
+
+    const slug = "first-post";
+    await writePostFiles({
+      slug,
+      meta: {
+        title: "This is the first post",
+        abstract: "This is the very first post.",
+        publish: true
+      },
+      content: [
+        "# This is the first post",
+        "It has some content."
+      ]
+    });
+
+    const publishDate = "2020-03-11";
+    advanceTo(publishDate);
+    await compilePosts();
+
+    const updatedDate = "2020-03-12";
+    advanceTo(updatedDate);
+    await compilePosts();
+
+    const manifest = await getPostManifest();
+
+    expect(manifest[slug].lastUpdateDate).toBeNull();
+  });
+
+  it("updates the lastUpdatedDate when the manifest needs to be fetched.", async () => {
+    await deleteDirectories(sourceDir, outputDir);
+
+    const slug = "first-post";
+    const postFile: PostFile = {
+      slug,
+      meta: {
+        title: "This is the first post",
+        abstract: "This is the very first post.",
+        publish: true
+      },
+      content: [
+        "# This is the first post",
+        "It has some content."
+      ]
+    };
+    await writePostFiles(postFile);
+
+    const oldManifestLocator = "https://www.oldManifest.com";
+    await compilePosts({ oldManifestLocator });
+    const manifest = await getPostManifest();
+
+    mocked(fetch).mockImplementation(() => {
+      return Promise.resolve({
+        json: (): Promise<unknown> => {
+          return Promise.resolve(manifest);
+        }
+      } as any);
+    });
+
+    await writePostFiles({
+      ...postFile,
+      content: [
+        ...postFile.content,
+        "some new content"
+      ]
+    });
+
+    const updatedDate = "2020-03-12";
+    advanceTo(updatedDate);
+    await compilePosts({ oldManifestLocator });
+
+    expect(fetch).toHaveBeenCalledWith(oldManifestLocator);
+
+    const newManifest = await getPostManifest();
+    expect(newManifest[slug].lastUpdateDate).toStrictEqual(
+      new Date(updatedDate).toUTCString()
+    );
   });
 });
