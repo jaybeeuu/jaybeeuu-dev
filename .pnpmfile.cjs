cd3f4210f2a7a9a8bfd88c8c814dc9a94fc5023e
Josh Bickley-Wallace <joshbickleywallace@outlook.com>
2023-04-21 15:08:38 +0100

    Pin @octokit/types to 9.0.0

--- .pnpmfile.cjs
+++ .pnpmfile.cjs
@@ -10,6 +10,15 @@ const readPackage = (pkg, context) => {
     context.log("markdown-spellcheck, dependency sinon-as-promised => devDependencies");
   }
 
+  // @octokit/types 9.1.0 is broken: https://github.com/octokit/types.ts/issues/525
+  if (pkg.name === "@octokit/core") {
+    pkg.dependencies = {
+      ...pkg.dependencies,
+      "@octokit/types": "9.0.0"
+    };
+    context.log("@octokit/core, dependency @octokit/types => pinned to 9.0.0");
+  }
+
   return pkg;
 }
 
