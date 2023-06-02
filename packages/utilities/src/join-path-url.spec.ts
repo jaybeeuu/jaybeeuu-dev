abacf8a49c427333a8ce7040a5c51fb5be074d6c
Josh Bickley-Wallace <joshbickleywallace@outlook.com>
2023-06-02 19:57:45 +0100

    add unit tests for join path url

---
+++ packages/utilities/src/join-path-url.spec.ts
@@ -0,0 +1,31 @@
+import { joinUrlPath } from "./join-url-path";
+
+describe("joinUrlPath", () => {
+  const samples: {
+    description: string;
+    input: string[];
+    expected: string;
+  }[] = [
+    {
+      description: "joins the fragments with /'s",
+      expected: "/this/is/a/path",
+      input: ["this", "is", "a", "path"]
+    },
+    {
+      description: "does not include leading and trailing /'s from the fragments",
+      expected: "/this/is/a/path",
+      input: ["this/", "is/", "/a", "/path/"]
+    },
+    {
+      description: "does includes middle /'s from the fragments",
+      expected: "/this/is/a/path",
+      input: ["this/is/", "/a", "/path/"]
+    }
+  ];
+  it.each(samples)(
+    "$#: $description",
+    ({ expected, input }) => {
+      expect(joinUrlPath(...input)).toBe(expected);
+    }
+  );
+});
