711444926011992c2e45194af3469991ed6d62d5
Josh Bickley-Wallace <2077433+jaybeeuu@users.noreply.github.com>
2024-07-12 23:43:01 +0100

    include conv in jest packages

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,13 +1,11 @@
 const config = {
   projects: [
     "<rootDir>/packages/compost",
-    "<rootDir>/packages/e2e-hooks",
+    "<rootDir>/packages/conv",
     "<rootDir>/packages/is",
-    "<rootDir>/packages/posts",
     "<rootDir>/packages/preact-async",
     "<rootDir>/packages/preact-recoilless",
     "<rootDir>/packages/recoilless",
-    "<rootDir>/packages/scripts",
     "<rootDir>/packages/site",
     "<rootDir>/packages/utilities",
   ],
